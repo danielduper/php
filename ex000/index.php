fccb3b31d7332c53357960d0ea28af56c7eb19dc
DDANIEL! <dmarttos8@gmail.com>
2025-07-28 15:00:05 -0300

    FIRST

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP GUANABAS</title>
+</head>
+<body>
+    <h2>Aqui é HTML!</h2>    
+    <?php
+        $nome = "Daniel";
+        echo "meu nome é $nome \u{1F480}";
+    ?>
+
+</body>
+</html>
